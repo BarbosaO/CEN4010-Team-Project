123773e46047b7b6bfa89b24fe0fb84d6937d147
Genesis Barrios <genesisbarriosdev@gmail.com>
2019-09-26 14:37:55 -0400

    database connection aion added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var MongoClient = require('mongodb').MongoClient;
 
 //npm install request - for making http calls
 //var request = require("request");
@@ -17,3 +18,12 @@ app.get("/", function(req, res){
 app.listen(3000, "localhost", function(){
     console.log("Listening on port 3000...")
 })
+
+MongoClient.connect("mongodb+srv://test1:test1@cluster0-jdush.azure.mongodb.net/test", function (err, db) {
+   
+     if(err) throw err;
+     else console.log('connected to database');
+
+     //Write databse Insert/Update/Query code here..
+                
+});
