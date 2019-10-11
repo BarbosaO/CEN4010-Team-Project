9bb0db515bca44310028b6dd7cd1eb08aa24e7c6
Xanderlae <Xanderlae@gmail.com>
2019-10-11 13:06:03 -0400

    db.collection(...).find({Insert field matches) to select items from database

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
-
+var url = "mongodb+srv://aagui:pw@cluster0-jdush.azure.mongodb.net/test";
 //npm install request - for making http calls
 //var request = require("request");
 
@@ -15,15 +16,27 @@ app.get("/", function(req, res){
 });
 
 // Server
+app.timeout = 0;
 app.listen(3000, "localhost", function(){
     console.log("Listening on port 3000...")
 })
 
-MongoClient.connect("mongodb+srv://test1:test1@cluster0-jdush.azure.mongodb.net/test", function (err, db) {
-   
-     if(err) throw err;
-     else console.log('connected to database');
-
-     //Write databse Insert/Update/Query code here..
-                
+MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) 
+{
+assert.equal(null, err);
+console.log("Successfully connected to server");
+var db = client.db('Test1');
+// Find some documents in our collection
+// Right now set to select books with Author: "John Doe" for testing
+db.collection('Test').find({Author: "John Doe"}).toArray(function(err, docs) {
+// Print the documents returned
+docs.forEach(function(doc) {
+console.log(doc.Title);
 });
+// Close the DB
+client.close();
+});
+// Declare success
+console.log("Called find()");
+ });
+ 
