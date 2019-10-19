74152c33bf22fa505df5043051e1be317bc73bc9
Xanderlae <Xanderlae@gmail.com>
2019-10-19 09:45:45 -0400

    Saving changes before merge

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://aagui:pw@cluster0-jdush.azure.mongodb.net/test";
+var db;
 //npm install request - for making http calls
 //var request = require("request");
 
@@ -26,24 +27,31 @@ MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true}, func
 {
 	//Verify connection
 	assert.equal(null, err);
-	console.log("Successfully connected to server");
-	var db = client.db('Test1');
-	//This line sets up url for this section .../booksList
-	app.get('/booksList', function(req, res){
-		//This ling below gets all items in the Test collection, can filter it with input in the find({*filter elements*}) part
-		db.collection('Test').find({}).toArray(function(err, docs) {
-			//Print the documents returned on console in this commented 3 line part
-				//docs.forEach(function(doc) {
-				//console.log(doc.Title);
-				//});
-			//Next line sends the list of items from collection accessed to the render
-			res.render("pages/bookList.ejs", {docs: docs});
-				
-			//Close the DB
-			client.close();
-		});
-	});
 	//Declare success
-	console.log("Called find()");
- });
+	console.log("Successfully connected to server");
+	db = client.db('Test1');
+
+});
+
+//This line sets up url for this section .../booksList
+app.get('/booksList', function(req, res){
+	//This ling below gets all items in the Test collection, can filter it with input in the find({*filter elements*}) part
+	db.collection('Test').find({Author: "John Doe"}).toArray(function(err, docs) {
+		//Print the documents returned on console in this commented 3 line part
+			//docs.forEach(function(doc) {
+			//console.log(doc.Title);
+			//});
+		//Next line sends the list of items from collection accessed to the render
+		res.render("pages/bookList.ejs", {docs: docs});
+	});
+});
+/*
+app.post('/booksListSort', function(req, res){
+	var genre = req.body.book_genre;
+
+	db.collection("Test").find({Genre: genre}).toArray(function(err, docs){
+		res.render("pages/bookList.ejs", {docs: docs});
+	});
+});
+*/
  
